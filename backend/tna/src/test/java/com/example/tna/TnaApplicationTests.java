14c9cdd3fd71e646be05fcc931810288bffad214
KeisukeYunoki <qifuyoumu@gmail.com>
2026-03-24 09:32:51 +0900

    spring

---
+++ backend/tna/src/test/java/com/example/tna/TnaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tna;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TnaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
